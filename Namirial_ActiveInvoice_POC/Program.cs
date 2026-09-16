c270de15a1793302b8086c55bdc2db0bbbea6d03
Simone Cammarano <simone.cammarano@infominds.eu>
2026-09-16 12:10:36 +0200

    GitIgnore

--- Namirial_ActiveInvoice_POC/Program.cs
+++ Namirial_ActiveInvoice_POC/Program.cs
@@ -92,7 +92,7 @@ app.MapGet("/SendInvoice", async (string transactionId, CancellationToken ct) =>
             client => client.SendElectronicInvoiceAsync(auth, new SendInvoice()
             {
                 IdTransaction = transactionId,
-                ToSign = false,
+                ToSign = true,
                 Filename = "IT59502613140_0000000003.xml"
             }),
             operationName: nameof(SolutionDOC_HubSoapClient.InsertDraftElectronicInvoiceAsync), ct: ct);
